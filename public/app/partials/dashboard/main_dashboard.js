fb00be5105a18e3e26a0d1d9272c194a3302131e
ira <ira@debian>
2015-11-13 11:58:51 +0600

    add env var chart and devide one dashboard into two with different logic

---
+++ public/app/partials/dashboard/main_dashboard.js
@@ -0,0 +1,74 @@
+'use strict';
+
+var app = angular.module('testReport.mainDashboard', [
+    'ngRoute',
+    'testReportServices',
+    'testReportServicesDashboard',
+    'highcharts-ng'
+]);
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/dashboard', {
+        templateUrl: '/static/app/partials/dashboard/main_dashboard.html',
+        controller: 'MainDashboardCtrl'
+    });
+}]);
+
+app.controller('MainDashboardCtrl', ['$scope', '$rootScope', 'appConfig', 'Project', 'TestPlan', 'Launch', 'Filters', 'UpdateLaunches', 'FilterLaunches', 'GetChartsData', 'SeriesStructure', 'Tooltips', 'GetChartStructure',
+    function ($scope, $rootScope, appConfig, Project, TestPlan, Launch, Filters, UpdateLaunches, FilterLaunches, GetChartsData, SeriesStructure, Tooltips, GetChartStructure) {
+        $scope.default_days = appConfig.DEFAULT_DAYS;
+
+        Project.query(function (response) {
+            $scope.projects = response.results;
+            fetchData();
+        });
+
+        function fetchData() {
+            _.each($scope.projects, function (project) {
+                TestPlan.get({ projectId: project.id }, function (response) {
+                    project.testplans = _.filter(response.results, Filters.showOnDashboard);
+                    project.testplans = _.filter(project.testplans, Filters.removeHidden);
+                    project.testplans = _.sortBy(project.testplans, 'name');
+
+                    _.each(project.testplans, function (testplan) {
+                        $scope.addChartsToTestplan(testplan, appConfig.DEFAULT_DAYS);
+                    });
+                });
+            });
+        }
+
+        $scope.addChartsToTestplan = function(testplan, days) {
+            testplan.days = days;
+            Launch.custom_list({
+                testPlanId: testplan.id,
+                state: appConfig.LAUNCH_STATE_FINISHED,
+                days: days,
+                search: testplan.filter
+            }, function (response) {
+                testplan.charts = [];
+
+                //launches for common chart by date
+                var launches = UpdateLaunches.cutDate(response.results);
+                launches = FilterLaunches.byDate(launches);
+                launches = UpdateLaunches.addStatisticData(launches);
+                launches = _.sortBy(launches, 'id');
+
+                var seriesData = GetChartsData.series(launches);
+                var labels = GetChartsData.labels(launches);
+
+                testplan.charts.push(
+                    GetChartStructure(
+                        labels,
+                        SeriesStructure.getFailedAndSkipped(seriesData.failed, seriesData.skipped)
+                    ));
+
+                testplan.charts.push(
+                    GetChartStructure(
+                        labels,
+                        SeriesStructure.getTotal(seriesData.total),
+                        Tooltips.total()
+                    ));
+            });
+        };
+    }
+]);
